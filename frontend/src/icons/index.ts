64513baf6293d05ed68cb1f08577c6e8d366bca3
Erwann RAMBEAUX <erwann.rambeaux@icloud.com>
2025-11-19 11:54:46 +0100

    Feat: Version 1.0 [clean directory]

--- frontend/src/icons/index.ts
+++
@@ -1,8 +0,0 @@
-import PuzzleRushCmp from '../assets/icons/game-type-puzzle-rush.svg?component'
-import PuzzleRushRaw from '../assets/icons/game-type-puzzle-rush.svg?raw'
-
-export const Icons = {
-  puzzleRush: { component: PuzzleRushCmp, raw: PuzzleRushRaw },
-} as const
-
-export type IconKey = keyof typeof Icons
